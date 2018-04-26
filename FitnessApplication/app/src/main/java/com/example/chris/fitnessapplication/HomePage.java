a215dec2424eb8b1f43852dbb4f7fd430856a9cb
invigorant <theinvigorantdesign@gmail.com>
2018-04-26 09:17:46 +0100

    added comparsion

--- FitnessApplication/app/src/main/java/com/example/chris/fitnessapplication/HomePage.java
+++ FitnessApplication/app/src/main/java/com/example/chris/fitnessapplication/HomePage.java
@@ -24,6 +24,8 @@ import android.support.v4.app.Fragment;
 
 import com.example.chris.fitnessapplication.data.Exercises.ExerciseDetailsDatabase;
 import com.example.chris.fitnessapplication.data.Exercises.ExercisesDetails;
+import com.example.chris.fitnessapplication.data.Users.UserDetails;
+import com.example.chris.fitnessapplication.data.Users.UserDetailsDatabase;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -317,7 +319,18 @@ public class HomePage extends Fragment{
 
         for ( ExercisesDetails temp: currentExercise)
         {
-            listToBeFilled.add(temp.getName());
+            UserDetails dataInput =  UserDetailsDatabase.getInstance(getActivity()).UserDetailsDao().getUserById(2);
+            ArrayList<String> disabilityTagsForUsers = dataInput.getDisabilityTags();
+            ArrayList<String> disabilityTagsForExercises = temp.getDisabilityTags();
+
+            List<String> common = new ArrayList<String>(disabilityTagsForUsers);
+            common.retainAll(disabilityTagsForExercises);
+
+            if(common.isEmpty())
+            {
+                listToBeFilled.add(temp.getName());
+            }
+
         }
 
         return  listToBeFilled;
